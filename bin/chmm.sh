49483ef94aaa25c8a160163caf6be72d7176c6b0
CodyFromIT <smith.11595@osu.edu>
2017-06-10 22:34:42 -0400

    Add 1/2 and 2/1 2/2.
    Added verify app updates, disable bluetooth if no device is connected,
    and show bluetooth in menubar.

--- bin/chmm.sh
+++ bin/chmm.sh
@@ -6,7 +6,9 @@ BASEDIR=$HOME/chmm
 LOG=$HOME/chmm/logs/log
  
 section_1(){
-    
+   
+    # Section 1 - Software Updates and Verification
+ 
     echo $TIME >> $LOG
     echo "BEGINING SECTION 1 - SOFTWARE VERIFICATION" >> $LOG
 
@@ -24,7 +26,62 @@ section_1(){
         fi
     fi
 
-    ####
+    # Check to verify App Updates
+    if [ $(defaults read /Library/Preferences/com.apple.commerce AutoUpdate) -eq 1 ]; then
+        echo "[ PASS ] App Update Installs" >> $LOG
+    else
+        defaults write /Library/Preferences/com.apple.commerce AutoUpdate -bool TRUE
+        
+        if [ $(defaults read /Library/Preferences/com.apple.commerce AutoUpdate) -eq 1 ]; then
+            echo "[ PASS - FIXED ] App Update Installs" >> $LOG
+        else
+            echo "[ FAILED ] App Update Installs" >> $LOG
+        fi
+    fi
+
+    echo $TIME >> $LOG
+    echo "SECTION 1 COMPLETE" >> $LOG
+}
+
+section_2(){
+    
+    # Section 2 - General System Security Practices
+    
+    echo $TIME >> $LOG
+    echo "BEGINING SECTION 2 - GENERAL SYSTEM SECURITY" >> $LOG
+
+    # Bluetooth is Disabled unless a Device is Connected 
+    if [ "$(defaults read /Library/Preferences/com.apple.Bluetooth ControllerPowerState)" -eq "0" ]; then
+        echo "[PASSED] 2.1.1 Bluetooth is disabled" >> $LOG
+
+    else
+        if [ "$(defaults read /Library/Preferences/com.apple.Bluetooth ControllerPowerState)" -eq "1" ]; then
+            result=$(system_profiler SPBluetoothDataType | grep "Bluetooth:" -A 20 | grep Connectable)
+            if [ "$result" == "          Connectable: Yes" ]; then
+                echo "[PASSED] 2.1.1 Bluetooth is Disabled unless Paired" >> $LOG
+            else
+                echo "[FAILED] 2.1.1 Bluetooth is not Disabled and no Device Connected." >> $LOG
+            fi
+        else
+            echo "General Failure...." >> $LOG
+        fi
+    fi
+    
+    # Show bluetooth status in menubar
+
+    if defaults read com.apple.systemuiserver menuExtras | grep -q Bluetooth.menu ; then
+        echo "[PASSED] 2.1.3 Show bluetooth status in menubar" >> $LOG
+    else
+       echo "[FAILED] 2.1.3 Show bluetooth status in menubar." >> $LOG
+        defaults write com.apple.systemuiserver menuExtras -array-add "/System/Library/CoreServices/Menu Extras/Bluetooth.menu"
+
+        if defaults read com.apple.systemuiserver menuExtras | grep -q Bluetooth.menu ; then
+            echo "[FIXED] 2.1.3 Show bluetooth status in menubar" >> $LOG
+        else
+            echo "[FAILURE] 2.1.3 Show bluetooth status in menubar" >> $LOG
+        fi
+    fi
+
 }
 
 main(){
@@ -32,6 +89,7 @@ main(){
     echo $TIME >> $HOME/chmm/logs/log
     echo "BEGINING CANTHACKMYMAC" >> $HOME/chmm/logs/log
     section_1
+    section_2
 
 }
 
